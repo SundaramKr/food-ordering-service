198d560274cb7b4e6d3d588a4505c5e58444b7a7
Sundaram <sundaramk25000@gmail.com>
2024-09-21 12:46:57 +0530

    Added Lombok

--- src/main/java/com/food/Restaurant/entity/Restaurant.java
+++ src/main/java/com/food/Restaurant/entity/Restaurant.java
@@ -1,64 +1,22 @@
 package com.food.Restaurant.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "restaurants")
+@Data
+@NoArgsConstructor
 public class Restaurant {
     @Id
     private ObjectId id;
+    @NonNull
     private String name;
     private String imgLink;
     private String[] cuisine;
     private String location;
     private Double ratings;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImgLink() {
-        return imgLink;
-    }
-
-    public void setImgLink(String imgLink) {
-        this.imgLink = imgLink;
-    }
-
-    public String[] getCuisine() {
-        return cuisine;
-    }
-
-    public void setCuisine(String[] cuisine) {
-        this.cuisine = cuisine;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Double getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(Double ratings) {
-        this.ratings = ratings;
-    }
 }
